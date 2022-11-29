61c1e8974f8d3e9fc71e96dadc41b391013ae404
annoinspace <aneesahalmask@gmail.com>
2022-11-29 07:14:47 +0000

    simple counter using setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState } from "react"
 
 function App() {
+  const [count, setCount] = useState(4)
+  function decreaseCount() {
+    setCount((prevCount) => prevCount - 1)
+  }
+  function increaseCount() {
+    setCount((prevCount) => prevCount + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={decreaseCount}>-</button>
+      <span>{count}</span>
+      <button onClick={increaseCount}>+</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
